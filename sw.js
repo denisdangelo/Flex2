047cad5f727d7fd1c0b43c6a4384a4054a9a9bcf
DenisDS <denis.dsilveira1@outlook.com>
2025-02-11 22:14:50 -0300

    atualização do site

---
+++ sw.js
@@ -0,0 +1,42 @@
+/**
+ * Service Worker
+ * @author Denis D'Angelo
+ */
+
+//instalação (cache - armazenamento local)
+self.addEventListenner('install', (event) => {
+    event.waitUntil(
+        caches.open('static')
+            .then((cache) => {  
+                cache.add('/flexv2/')
+                cache.add('/flexv2/index.html')
+                cache.add('/flexv2/style.css')
+                cache.add('/flexv2/app.js')
+                cache.add('/flexv2/img/flex.png')
+                cache.add('/flexv2/img/calcflex.png')
+                cache.add('/flexv2/img/etanol.png')
+                cache.add('/flexv2/img/gasolina.png')
+        })
+    )
+})
+
+//ativação 
+self.addEventListenner('activate', (event) => {
+    console.log('Service Worker ativado', event)
+    return self.clients.claim()
+})
+
+
+//interceptação (solicitações https servindo em cache quando ofline)
+self.addEventListenner('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+              if(response) {
+                  return response
+              } else {
+                  return fetch(event.request)
+              }
+            })
+    )
+})
